248fd0de4d3548689528475176e3570c55c80329
matveyb <matvey.b2022@gmail.com>
2025-09-10 14:34:02 +0300

    refactore styles + vars new

--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { everscaleNetwork, everWallet, sparxWallet, TvmConnectUI, venomWallet } from '../../../dist/esm'
+import { everscaleNetwork, everWallet, sparxWallet, TvmConnectUI, venomWallet } from '@broxus/tvm-connect-ui'
 import '@broxus/tvm-connect-ui/styles.css'
 
 const tvmConnectUI = new TvmConnectUI({
